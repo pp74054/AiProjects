91063f41356439c3aa2bf5d639f6bce0eaf48f7c
pp74054 <praksha711@gmail.com>
2025-09-02 12:36:50 -0700

    appended

--- LangChain_Project/RAG/rag.py
+++ LangChain_Project/RAG/rag.py
@@ -11,6 +11,12 @@ from llama_index.core.node_parser import SentenceSplitter
 splitter = SentenceSplitter(chunk_size=1024)
 nodes = splitter.get_nodes_from_documents(documents)
 
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+settings.llm = OpenAI(model = "gpt-3.5-turbo")
+settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
 
 
 
